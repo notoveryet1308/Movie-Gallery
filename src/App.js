8a47755d47ef20f09b97f14b1b531f708f5475bd
Rahul Raj <rahulraz1308@gmail.com>
2020-12-10 11:01:12 +0530

    making PWA in progress

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
 import {AppContainer} from './GlobalStyle';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import Homepage from './pages/Home/Homepage';
 import Moviedetail from './pages/Moviedetail/Moviedetail';
 import {useSelector} from 'react-redux';
-import Popular from './pages/Popular/Popular';
-import TopRated from './pages/TopRated/TopRated';
-import Upcoming from './pages/Upcoming/Upcoming';
-import Discover from './pages/Discover/Discover';
-import Search from './pages/Search/Search';
 
+// import Popular from './pages/Popular/Popular';
+// import TopRated from './pages/TopRated/TopRated';
+// import Upcoming from './pages/Upcoming/Upcoming';
+// import Discover from './pages/Discover/Discover';
+// import Search from './pages/Search/Search';
+
+const Popular = lazy(() => import('./pages/Popular/Popular'));
+const TopRated = lazy(() => import('./pages/TopRated/TopRated'))
+const Upcoming = lazy(() => import('./pages/Upcoming/Upcoming'))
+const Discover = lazy(() => import('./pages/Discover/Discover'))
+const Search = lazy(() => import('./pages/Search/Search'))
 
 function App() {
   const currMovieDetail = useSelector(state=> state.moviedetail.movie)
   return (
     <AppContainer>
       <Switch>
-         <Route exact path='/' component={Homepage}/>
+        <Suspense fallback={<Loading />}>
+           <Route exact path='/' component={Homepage}/>
          <Route 
             path='/detail/:moviename' 
             render={()=> 
@@ -26,6 +33,8 @@ function App() {
           <Route exact path='/upcoming' component={Upcoming}/>
           <Route exact path='/discover' component={Discover}/>
           <Route exact path='/search' component={Search}/>
+        </Suspense>
+        
 
       </Switch>
     </AppContainer>
@@ -34,3 +43,12 @@ function App() {
 }
 
 export default App;
+
+
+const Loading = ()=>{
+  return(
+    <div>
+      Loading....
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import {GlobalStyle} from './GlobalStyle';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {store} from './redux/store';
+
+import * as serviceWorker from './serviceWorker';
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -17,5 +20,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
 
 
